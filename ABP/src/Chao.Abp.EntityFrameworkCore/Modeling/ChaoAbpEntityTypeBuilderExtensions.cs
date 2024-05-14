887064919a40109f9f98ae74a7c6573a8107d4d8
杨超 <nankingcigar@163.com>
2024-05-14 21:51:09 +0800

    1. upgrade .net/abp/angular 2. add custom EF converter for ExtraProperty

---
+++ ABP/src/Chao.Abp.EntityFrameworkCore/Modeling/ChaoAbpEntityTypeBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Chao.Abp.EntityFrameworkCore.ValueConverter;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using Volo.Abp.Data;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+using Volo.Abp.EntityFrameworkCore.ValueComparers;
+
+namespace Chao.Abp.EntityFrameworkCore.Modeling;
+
+public static class ChaoAbpEntityTypeBuilderExtensions
+{
+    public static void ConfigureByConvention_Chao(this EntityTypeBuilder b)
+    {
+        b.TryConfigureExtraProperties();
+    }
+
+    public static void TryConfigureExtraProperties(this EntityTypeBuilder b)
+    {
+        if (!b.Metadata.ClrType.IsAssignableTo<IHasExtraProperties>())
+        {
+            return;
+        }
+
+        b.Property<ExtraPropertyDictionary>(nameof(IHasExtraProperties.ExtraProperties))
+            .HasColumnName(nameof(IHasExtraProperties.ExtraProperties))
+            .HasConversion(new ChaoExtraPropertiesValueConverter(b.Metadata.ClrType))
+            .Metadata.SetValueComparer(new ExtraPropertyDictionaryValueComparer());
+
+        b.TryConfigureObjectExtensions();
+    }
+}
---
+++ ABP/src/Chao.Abp.EntityFrameworkCore/ValueConverter/ChaoExtraPropertiesValueConverter.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Volo.Abp.Data;
+using Volo.Abp.Json.SystemTextJson.JsonConverters;
+using Volo.Abp.ObjectExtending;
+
+namespace Chao.Abp.EntityFrameworkCore.ValueConverter;
+
+public class ChaoExtraPropertiesValueConverter : ValueConverter<ExtraPropertyDictionary, string>
+{
+    public static readonly JsonSerializerOptions DeserializeOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+    {
+        Converters ={
+            new ObjectToInferredTypesConverter()
+        }
+    };
+
+    public static readonly JsonSerializerOptions SerializeOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+    public ChaoExtraPropertiesValueConverter(Type entityType)
+            : base(
+        d => SerializeObject(d, entityType),
+        s => DeserializeObject(s, entityType))
+    {
+    }
+
+    private static ExtraPropertyDictionary DeserializeObject(string extraPropertiesAsJson, Type? entityType)
+    {
+        if (extraPropertiesAsJson.IsNullOrEmpty() || extraPropertiesAsJson == "{}")
+        {
+            return new ExtraPropertyDictionary();
+        }
+
+        var dictionary = JsonSerializer.Deserialize<ExtraPropertyDictionary>(extraPropertiesAsJson, DeserializeOptions) ??
+                            new ExtraPropertyDictionary();
+
+        if (entityType != null)
+        {
+            var objectExtension = ObjectExtensionManager.Instance.GetOrNull(entityType);
+            if (objectExtension != null)
+            {
+                foreach (var property in objectExtension.GetProperties())
+                {
+                    dictionary[property.Name] = GetNormalizedValue(dictionary!, property);
+                }
+            }
+        }
+
+        return dictionary;
+    }
+
+    private static object? GetNormalizedValue(
+    Dictionary<string, object> dictionary,
+    ObjectExtensionPropertyInfo property)
+    {
+        var value = dictionary.GetOrDefault(property.Name);
+        if (value == null)
+        {
+            return property.GetDefaultValue();
+        }
+
+        try
+        {
+            if (property.Type.IsEnum)
+            {
+                return Enum.Parse(property.Type, value.ToString()!, true);
+            }
+
+            //return Convert.ChangeType(value, property.Type);
+            return value;
+        }
+        catch
+        {
+            return value;
+        }
+    }
+
+    private static string SerializeObject(ExtraPropertyDictionary extraProperties, Type? entityType)
+    {
+        var copyDictionary = new Dictionary<string, object?>(extraProperties);
+
+        if (entityType != null)
+        {
+            var objectExtension = ObjectExtensionManager.Instance.GetOrNull(entityType);
+            if (objectExtension != null)
+            {
+                foreach (var property in objectExtension.GetProperties())
+                {
+                    if (property.IsMappedToFieldForEfCore())
+                    {
+                        copyDictionary.Remove(property.Name);
+                    }
+                }
+            }
+        }
+
+        return JsonSerializer.Serialize(copyDictionary, SerializeOptions);
+    }
+}
