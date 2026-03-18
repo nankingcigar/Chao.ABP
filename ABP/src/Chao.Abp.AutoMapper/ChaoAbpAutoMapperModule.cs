807063ce4812ef0be8aef202b6aab5778f263f10
杨超 <yangchao@tuo-fan.com>
2026-03-18 12:42:24 +0800

    bug fix

--- ABP/src/Chao.Abp.AutoMapper/ChaoAbpAutoMapperModule.cs
+++ ABP/src/Chao.Abp.AutoMapper/ChaoAbpAutoMapperModule.cs
@@ -1,4 +1,10 @@
-﻿using Volo.Abp.AutoMapper;
+﻿using AutoMapper;
+using AutoMapper.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
 
 namespace Chao.Abp.AutoMapper;
@@ -8,4 +14,33 @@ namespace Chao.Abp.AutoMapper;
     )]
 public class ChaoAbpAutoMapperModule : AbpModule
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddSingleton<IConfigurationProvider>(sp =>
+        {
+            using (var scope = sp.CreateScope())
+            {
+                var options = scope.ServiceProvider.GetRequiredService<IOptions<AbpAutoMapperOptions>>().Value;
+
+                var mapperConfigurationExpression = sp.GetRequiredService<IOptions<MapperConfigurationExpression>>().Value;
+                var autoMapperConfigurationContext = new AbpAutoMapperConfigurationContext(mapperConfigurationExpression, scope.ServiceProvider);
+
+                foreach (var configurator in options.Configurators)
+                {
+                    configurator(autoMapperConfigurationContext);
+                }
+
+                var loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+
+                var mapperConfiguration = new MapperConfiguration(mapperConfigurationExpression, loggerFactory);
+
+                foreach (var profileType in options.ValidatingProfiles)
+                {
+                    mapperConfiguration.Internal().AssertConfigurationIsValid(((Profile)Activator.CreateInstance(profileType)!).ProfileName);
+                }
+
+                return mapperConfiguration;
+            }
+        });
+    }
 }
